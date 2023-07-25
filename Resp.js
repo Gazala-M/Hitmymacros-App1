6410615bfd51b98cc0fa36b1394f67470eecc225
Gazala-M <128143047+Gazala-M@users.noreply.github.com>
2023-07-25 22:22:01 +0530

    Add files via upload

---
+++ Resp.js
@@ -0,0 +1,125 @@
+/*burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+nav = document.querySelector('.nav')
+
+
+
+burger.addEventListener('click', ()=>{
+    
+    nav.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+
+    burger.classList.toggle("active");
+    nav.classList.toggle("active");
+})   */
+
+
+
+(function($) { 
+  $(function() { 
+
+    //  open and close nav 
+    $('#navbar-toggle').click(function() {
+      $('nav ul').slideToggle();
+    });
+
+
+    // Hamburger toggle
+    $('#navbar-toggle').on('click', function() {
+      this.classList.toggle('active');
+    });
+
+
+    // If a link has a dropdown, add sub menu toggle.
+    $('nav ul li a:not(:only-child)').click(function(e) {
+      $(this).siblings('.navbar-dropdown').slideToggle("slow");
+
+      // Close dropdown when select another dropdown
+      $('.navbar-dropdown').not($(this).siblings()).hide("slow");
+      e.stopPropagation();
+    });
+
+
+    // Click outside the dropdown will remove the dropdown class
+    $('html').click(function() {
+      $('.navbar-dropdown').hide();
+    });
+  }); 
+})(jQuery); 
+/*window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    navbar.classList.add("navbar-scrolled");
+  } else {
+    navbar.classList.remove("navbar-scrolled");
+  }
+});   */
+
+/*
+  window.addEventListener("scroll", function(){
+    var header = this.document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+  })  */
+
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+
+const email = document.getElementById("email");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  validateInputs();
+
+});
+
+const setError = (element, message) => {
+  const formfield = element.parentElement;
+  const errorDisplay = formfield.querySelector('.error');
+
+  errorDisplay.innerText = message;
+  formfield.classList.add('error');
+}  
+
+const validateInputs = () => {
+  const usernameValue = username.value.trim();
+  const emailValue = email.value.trim();
+
+
+if (usernameValue === '') {
+ setError(username, 'Username is required');
+}
+else
+{
+  console.log('User Name: ' + usernameValue);
+}
+
+
+
+if (emailValue === "") {
+  setError(email, 'Email is required');
+}
+else
+{
+  console.log('Email: ' + emailValue);
+}
+
+
+
+};  
+
+
+// aos animation
+
+AOS.init();
+
+
+AOS.init({
+duration: 800,  // Animation duration in milliseconds
+offset: 200     // Offset (in pixels) from the original trigger point
+});
+
+AOS.init({
+useClassNames: true,  // Enables the use of classes for animation targets
+initClassName: 'aos-animate', // Class name to be animated
+
+});    
